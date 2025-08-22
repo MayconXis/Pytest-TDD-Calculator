ef00335a772a1c667f151c0de16740d3814a3b1e
Maycon Douglas <md7111016@gmail.com>
2025-08-22 09:13:30 -0300

    Commit do primeiro teste

---
+++ Pytest-TDD-Calculator/test_basic1.py
@@ -0,0 +1,7 @@
+def is_positive (numero) :
+    return numero > 0 
+def teste_eh_positivo() :
+        assert is_positive(5) is False
+        assert is_positive(-5) is False
+        
+#para is_positive como False 
