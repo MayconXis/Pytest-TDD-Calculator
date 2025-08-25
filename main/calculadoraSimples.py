3ef1740629ce6b2f5b2dc32e67236bf8f4c03229
Maycon Douglas <md7111016@gmail.com>
2025-08-25 11:54:41 -0300

    Commit de implementação do CI/CD

---
+++ main/calculadoraSimples.py
@@ -0,0 +1,6 @@
+#Esta calculadora foi feita apenas para aprendizado e não esta relacionada ao projeto
+num1 = input ('Digite o primeiro numero: ')
+num2 = input ('Digite o segundo numero: ')
+resultado = int(num1) + int(num2)
+
+print(resultado)
